b9289ce29bcf9a5ea56379ef1b40fd2839137ea0
MahmoudALamMohamed <mahmoudalm2019@gmail.com>
2025-12-16 16:44:08 +0200

    edit

--- src/pages/Restaurant.tsx
+++ src/pages/Restaurant.tsx
@@ -290,12 +290,12 @@ ${orderText}
                 <span>{restaurant.address}</span>
               </div>
              )} */}
-            {restaurant.facebook_url && <a href={restaurant.facebook_url} target="_blank" rel="noopener noreferrer" className="w-8 h-8 bg-gray-800 rounded-lg flex items-center justify-center">
+            {restaurant.facebook_url && <div className="w-8 h-8 bg-gray-800 rounded-lg flex items-center justify-center">
                 <Facebook className="w-4 h-4" />
-              </a>}
-            {restaurant.instagram_url && <a href={restaurant.instagram_url} target="_blank" rel="noopener noreferrer" className="w-8 h-8 bg-gray-800 rounded-lg flex items-center justify-center">
+              </div>}
+            {restaurant.instagram_url && <div className="w-8 h-8 bg-gray-800 rounded-lg flex items-center justify-center">
                 <Instagram className="w-4 h-4" />
-              </a>}
+              </div>}
             
           </div>
         </div>
